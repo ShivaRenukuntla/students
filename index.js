df675da9e35ac7b1e35a53f4d0bd153c39969184
ShivaRenukuntla <shivasurya.prasad4@gmail.com>
2018-08-15 19:07:38 +0530

    Refactored

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var middlewares = require('./middlewares');
-var trueLog  = require('true-log');
+var trueLog = require('true-log');
 var fs = require('fs');
 var defaultRouter = require('./Routers/default.router');
 var studentsRouter = require('./Routers/students.router');
@@ -12,30 +12,38 @@ var reviewRouter = require('./Routers/review.router');
 
 var port = process.env.PORT || 80;
 
-app.listen(port,function(){
+app.listen(port, function () {
     console.log("I am running the server on ", port);
 })
 
-try{
-    mongoose.connect("mongodb://admin:admin123@ds123012.mlab.com:23012/studentsdb",{ useNewUrlParser: true }, function(){
-        console.log("I am running the Db on 27017");
-    })
+try {
+    mongoose.connect("mongodb://admin:admin123@ds123012.mlab.com:23012/studentsdb", { useNewUrlParser: true, 
+    socketTimeoutMS: 0,
+    connectTimeoutMS: 0,        
+    }
+    )
+        .then(function () {
+            console.log("Connected");
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
 }
-catch(err){
+catch (err) {
     console.log("Unable to connect to MongoDb");
 }
 
 
 
-var ws = fs.createWriteStream("logs/request.log", {flags: 'a'});
-app.use(trueLog({level: 'full', stream : ws}));
+var ws = fs.createWriteStream("logs/request.log", { flags: 'a' });
+app.use(trueLog({ level: 'full', stream: ws }));
 
 
 app.use(bodyParser.json());
 app.use(express.static("uploads"));
 app.use('/', defaultRouter);
-app.use('/api/users',userRouter);
+app.use('/api/users', userRouter);
 // // app.use(middlewares.authenticate);  
 // app.use(middlewares.validateToken);
-app.use('/students',studentsRouter);
-app.use('/reviews',reviewRouter );
+app.use('/students', studentsRouter);
+app.use('/reviews', reviewRouter);
